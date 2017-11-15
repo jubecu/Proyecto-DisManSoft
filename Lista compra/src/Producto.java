42d708423c41dac765f5b27aa23f265844f0abba
JuanfranBenito <jbc0056@alu.ubu.es>
2017-11-15 20:07:12 +0100

    Nueva clase Producto

---
+++ Lista compra/src/Producto.java
@@ -0,0 +1,16 @@
+
+public class Producto {
+
+	private String nombre;
+	
+	private float precio;
+	
+	public Producto(String nombre, float precio){
+		this.nombre=nombre;
+		this.precio=precio;
+	}
+	
+	public float obtenerPrecio(){
+		return precio;
+	}
+}
