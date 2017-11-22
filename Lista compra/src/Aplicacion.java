7c3c922a7802812bac3169d31f8ce7bb0f627904
JuanfranBenito <jbc0056@alu.ubu.es>
2017-11-22 20:42:23 +0100

    Añadir productos a la lista

--- Lista compra/src/Aplicacion.java
+++ Lista compra/src/Aplicacion.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class Aplicacion {
 
 	private static Lista lista;
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		lista=new Lista("Lista.txt");
 		lista.mostrarLista();
+		System.out.println("Introduce un producto:");
+		String nombre = sc.nextLine();
+		Producto producto=new Producto(nombre);
+		lista.añadirProducto(producto.obtenerNombre());
+		lista.mostrarLista();
+		sc.close();
 	}
 
 }
